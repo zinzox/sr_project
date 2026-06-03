c975e9d139f99be78c70919509faf9d8bdf1c847
zinzox <mohamedaziz.weslati@esen.tn>
2026-06-03 12:12:10 +0100

    Fix invalid Next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
 };
 
 module.exports = nextConfig;
